4737abf1923c97d78dff0b12de971fbbd7429a55
Mery Milagros Paco Ramos <mirakel1401@gmail.com>
2020-03-30 14:18:01 -0500

    Revert "Feature/primary components"

--- src/components/AppIcon/AppIcon.stories.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable */
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import AppIcon from './AppIcon';
-
-storiesOf('AppIcon', module).add('default', () => <AppIcon />);
--- src/components/LineAreaGraphic/LineAreaGraphic.stories.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable */
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import LineAreaGraphic from './LineAreaGraphic';
-
-storiesOf('LineAreaGraphic', module).add('default', () => <LineAreaGraphic />);
