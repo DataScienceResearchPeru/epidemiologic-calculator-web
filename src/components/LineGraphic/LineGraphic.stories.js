273353b9b585f7468f11027c2770c9bcbd9784e7
Randy Real <randyterros@gmail.com>
2020-03-31 21:39:14 -0500

    update components

---
+++ src/components/LineGraphic/LineGraphic.stories.js
@@ -0,0 +1,6 @@
+/* eslint-disable */
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import LineGraphic from './LineGraphic';
+
+storiesOf('LineGraphic', module).add('default', () => <LineGraphic />);
