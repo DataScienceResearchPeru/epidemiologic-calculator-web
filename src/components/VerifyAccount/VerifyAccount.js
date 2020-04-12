bd309085f3bd96e6e0e57c0d333a47ad2ad0f25c
Milagros <mirakel1401@gmail.com>
2020-04-12 00:09:16 -0500

    agregando el componente message y cambios en verificar cuenta

--- src/components/VerifyAccount/VerifyAccount.js
+++ src/components/VerifyAccount/VerifyAccount.js
@@ -1,49 +1,58 @@
 import React from 'react'
 import { useResource } from 'react-request-hook'
+import { Link } from 'react-navi'
 import PropTypes from 'prop-types'
 import { Container, Box } from '@material-ui/core'
-import { Link } from 'react-navi'
+import { makeStyles } from '@material-ui/core/styles'
 
 import Footer from '../Footer/Footer'
 import Header from '../Header/Header'
 import { api } from '../../middleware/api'
+import Message from '../Message/Message'
 
-const MessageSuccess = () => {
-  return (
-    <Box>
-      <h2>Verificación confirmada</h2>
-      <p>Su dirección de email se ha verificado con éxito.</p>
-      <Link href={'/'}>ir al login</Link>
-    </Box>
-  )
-}
 
-const MessageFailed = () => {
-  return (
-    <Box>
-      <h2>Verificación Fallida</h2>
-      <p>El enlace no es válido o ha expirado.</p>
-      <Link href={'/'}>Volver al inicio</Link>
-    </Box>
-  )
-}
+const useStyles = makeStyles((theme) => ({
+  boxContent: {
+    borderRadius: 17,
+    backgroundColor: '#5ad6d6',
+    padding: '80px 100px',
+    fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+    color: '#FFF',
+    minHeight: 400,
+    '& h1': {
+      fontSize: '1.8em',
+      fontWeight: 500,
+    },
+    '& a': {
+      color: '#FFF',
+      fontWeight: 600
+    }
+  },
+}))
 
 const VerifyAccount = (props) => {
   const { token } = props
+  const classes = useStyles()
   const [ response ] = useResource(api.verifyAccount, [token])
   let message
 
   if (response.data) {
-    message = <MessageSuccess />
+    message = <Message title="Verificación confirmada" 
+      description="Su dirección de email se ha verificado con éxito."
+      link="ir al login" />
   } else {
-    message = <MessageFailed />
+    message = <Message title="Verificación Fallida" 
+      description="El enlace no es válido o ha expirado."
+      link="Volver al inicio" />
   }
 
   return (
     <React.Fragment>
       <Container component="main" maxWidth="lg">
         <Header />
-        { message }
+        <Box className={classes.boxContent}>
+          { message }
+        </Box>
       </Container>
       <Footer />
     </React.Fragment>
